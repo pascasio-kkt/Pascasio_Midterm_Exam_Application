d0fb3a1f593b3f8c1a06fbbc2803a6725c86d387
Pascasio Krel <ktp9380@students.uc-bcf.edu.ph>
2025-02-24 13:52:32 +0000

    Update: 	Implemented an array in the controller based on the chosen theme

--- Pascasio_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
+++ Pascasio_Midterm_Exam_Application/app/Http/Controllers/ProductController.php
@@ -6,18 +6,67 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-        $animeProducts = [
-            'Given',
-            'No.6',
-            'Yuri on Ice',
-            'Doukyuusei (Classmates)',
-            'Sasaki to Miyano',
-            'Hitorijime My Hero',
-            'Banana Fish'
-        ];
+    public function showProducts()
+{
+    $animeProducts = [
+        [
+            'name' => 'Given', 
+            'author' => 'Natsuki Kizu', 
+            'year' => 2019, 
+            'price' => 100, 
+            'image' => 'https://example.com/images/given.jpg'
+        ],
+        [
+            'name' => 'Yuri on Ice', 
+            'author' => 'Sayo Yamamoto', 
+            'year' => 2016, 
+            'price' => 200, 
+            'image' => 'https://example.com/images/yuri_on_ice.jpg'
+        ],
+        [
+            'name' => 'Doukyuusei', 
+            'author' => 'Asumiko Nakamura', 
+            'year' => 2016, 
+            'price' => 300, 
+            'image' => 'https://example.com/images/doukyuusei.jpg'
+        ],
+        [
+            'name' => 'Sasaki to Miyano', 
+            'author' => 'Shou Harusono', 
+            'year' => 2022, 
+            'price' => 400, 
+            'image' => 'https://example.com/images/junjou_romantica.jpg'
+        ],
+        [
+            'name' => 'Hitorijime My Hero', 
+            'author' => ' Memeco Arii', 
+            'year' => 2017, 
+            'price' => 500, 
+            'image' => 'https://example.com/images/junjou_romantica.jpg'
+        ],
+        [
+            'name' => 'Banana Fish', 
+            'author' => 'Akimi Yoshida', 
+            'year' => 2018, 
+            'price' => 600, 
+            'image' => 'https://example.com/images/junjou_romantica.jpg'
+        ],
+        [
+            'name' => 'No. 6', 
+            'author' => 'Shungiku Nakamura', 
+            'year' => 2008, 
+            'price' => 700, 
+            'image' => 'https://example.com/images/junjou_romantica.jpg'
+        ],
+        [
+            'name' => 'The Stranger By The Beach', 
+            'author' => 'Kanna Kii', 
+            'year' => 2020, 
+            'price' => 800, 
+            'image' => 'https://example.com/images/junjou_romantica.jpg'
+        ],
+    ];
 
-        return view('welcome', compact('animeProducts'));
-    }
+    return view('welcome', compact('animeProducts'));
+}
 }
